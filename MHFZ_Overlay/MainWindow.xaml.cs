ac8fd2b0eab8bf86180887842794654573a6babe
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2022-11-06 10:04:30 -0300

    chore: bump version

--- MHFZ_Overlay/MainWindow.xaml.cs
+++ MHFZ_Overlay/MainWindow.xaml.cs
@@ -89,7 +89,7 @@ namespace MHFZ_Overlay
         public const int WS_EX_TRANSPARENT = 0x00000020;
         public const int GWL_EXSTYLE = (-20);
         //set version here
-        public const string CurrentProgramVersion = "v0.10.3";
+        public const string CurrentProgramVersion = "v0.10.4";
 
         [DllImport("user32.dll")]
         public static extern int GetWindowLong(IntPtr hwnd, int index);
