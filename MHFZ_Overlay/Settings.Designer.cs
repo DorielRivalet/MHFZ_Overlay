e06681af4c1b549e8485c6df6607f64842adc483
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-03-21 01:49:57 -0300

    feat(ui): add icons to configuration settings

--- MHFZ_Overlay/Settings.Designer.cs
+++ MHFZ_Overlay/Settings.Designer.cs
@@ -1273,7 +1273,7 @@ namespace MHFZ_Overlay {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool EnableMap {
             get {
                 return ((bool)(this["EnableMap"]));
