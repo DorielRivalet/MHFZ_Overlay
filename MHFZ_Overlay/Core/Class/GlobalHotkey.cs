043a45e9b7ab39d6aa9510d78b5c8087cc702d9e
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-05-26 23:54:16 -0300

    style: change copyright notice

--- MHFZ_Overlay/Core/Class/GlobalHotkey.cs
+++ MHFZ_Overlay/Core/Class/GlobalHotkey.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2023 The mhfz-overlay Authors.
+﻿// © 2023 The mhfz-overlay Authors.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 /*https://stackoverflow.com/a/65412682/18859245*/
--- MHFZ_Overlay/Core/Enum/Enums.cs
+++ MHFZ_Overlay/Core/Enum/Enums.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2023 The mhfz-overlay Authors.
+﻿// © 2023 The mhfz-overlay Authors.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 using System.Text.Json.Serialization;
