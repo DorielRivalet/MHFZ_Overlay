fd2b37334c2277cc9aa459432bc15325366299a9
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-06-14 22:39:09 -0300

    refactor: textboxes

--- MHFZ_Overlay/Settings.Designer.cs
+++ MHFZ_Overlay/Settings.Designer.cs
@@ -1921,7 +1921,7 @@ namespace MHFZ_Overlay {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool GamepadShown {
             get {
                 return ((bool)(this["GamepadShown"]));
