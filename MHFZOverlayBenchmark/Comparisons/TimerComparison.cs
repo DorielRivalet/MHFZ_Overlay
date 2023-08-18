7ded0ff75fcf3dadf8f3a05d01e4949ba674ec80
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-08-17 21:31:47 -0300

    feat(benchmark): add benchmarks

---
+++ MHFZOverlayBenchmark/Comparisons/TimerComparison.cs
@@ -0,0 +1,34 @@
+﻿// © 2023 The mhfz-overlay developers.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+namespace MHFZOverlayBenchmark.Comparisons;
+
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TimerComparison
+{
+    private const int N = 10000;
+    private readonly byte[] data;
+
+    private readonly SHA256 sha256 = SHA256.Create();
+    private readonly MD5 md5 = MD5.Create();
+
+    public TimerComparison()
+    {
+        data = new byte[N];
+        new Random(42).NextBytes(data);
+    }
+
+    [Benchmark]
+    public byte[] Sha256() => sha256.ComputeHash(data);
+
+    [Benchmark]
+    public byte[] Md5() => md5.ComputeHash(data);
+}
---
+++ MHFZOverlayBenchmark/Program.cs
@@ -0,0 +1,11 @@
+﻿// © 2023 The mhfz-overlay developers.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using System.Management;
+using MHFZOverlayBenchmark.Comparisons;
+
+var summary = BenchmarkRunner.Run<TimerComparison>();
+
