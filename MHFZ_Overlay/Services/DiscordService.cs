57e75bfb353e80583fb94e3050f39c5deffdd07e
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-06-19 09:44:14 -0300

    feat: update discord button link in template

--- MHFZ_Overlay/Services/DiscordService.cs
+++ MHFZ_Overlay/Services/DiscordService.cs
@@ -107,7 +107,7 @@ public sealed class DiscordService
         },
         Buttons = new Button[]
             {
-                new Button() { Label = "【MHF-Z】Overlay " + Program.CurrentProgramVersion, Url = "https://github.com/DorielRivalet/mhfz-overlay" },
+                new Button() { Label = "【MHF-Z】Overlay " + Program.CurrentProgramVersion, Url = "https://wycademy.vercel.app/overlay" },
                 new Button() { Label = "Discord RPC C# Dev Site", Url = "https://lachee.dev/" },
             },
     };
